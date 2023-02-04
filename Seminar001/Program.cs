28a217d575d1be122a9a02a125ed95b3714eac94
Evgeniy Kostromin <ewg.kostromin@yandexru>
2023-02-04 16:03:38 +0500

    первая зада ча семинара

---
+++ Seminar001/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Console.Clear();
+
+int num = new Random().Next(10, 100);
+Console.WriteLine($"Num = {num}");
+int num1 = num / 10;
+int num2 = num % 10;
+int max = num1;
+
+if (num2 > max)
+{
+  max = num2;
+}
+Console.WriteLine($"Max = {max} ");
