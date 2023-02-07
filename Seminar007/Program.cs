c76b75d217845f5c01b9a51ac5b68eb4d3921875
Evgeniy Kostromin <ewg.kostromin@yandexru>
2023-02-07 14:10:43 +0500

    seminar 007

---
+++ Seminar007/Program.cs
@@ -0,0 +1,53 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Clear();
+
+Console.Write("Введите номер четверти: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a == 1){
+  Console.WriteLine("x > 0 && y > 0");
+}else if (a == 2){
+  Console.WriteLine("x < 0 && y < 0");
+}else if (a == 3){
+  Console.WriteLine("x > 0 && y < 0");
+}else if (a == 4){
+  Console.WriteLine("x < 0 && y > 0");
+}else if (a > 4)
+{
+  Console.WriteLine("Неверный номер");
+}
+
+Console.WriteLine();
+
+Console.Write("Введите номер четверти: ");
+int quarter = int.Parse(Console.ReadLine()!); // решаем через switch
+
+switch (quarter)
+{
+  case 1:
+        {
+          Console.WriteLine("x > 0  y > 0");
+          break;
+        }
+  case 2:
+        {
+          Console.WriteLine("x < 0  y < 0");
+          break;
+        }
+  case 3:
+        {
+          Console.WriteLine("x > 0 y < 0");
+          break;
+        }
+  case 4:
+        {
+          Console.WriteLine("x < 0  y > 0");
+          break;
+        }                  
+  default:
+        {
+          Console.WriteLine("Введена неправильная четверть");
+          break;
+        }
+}
